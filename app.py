7e084ac379f7843cf30cb769166c7b4545509565
karthikkatakam2312 <karthikkatakam.23@gmail.com>
2024-03-01 19:40:20 -0500

    Made Code readable and Updated code convections

--- app.py
+++ app.py
@@ -3,36 +3,26 @@ import argparse
 import os
 import itertools
 
+items_info = {}
+
+# Function to read JSON data from a file
 def read_json_from_file(file_path):
     with open(file_path, 'r') as file:
         orders = json.load(file)
         return orders
 
+# Function to extract data from jsonfile and update customer and item information
 def extractingdata(orders_data):
     for order in orders_data:
-    #     name = order['name']
-    #     phone = order['phone']
-    #     print(f"Name extracted from order: {name}")
-    #     print(f"Phone extracted from order: {phone}")
         add_customer(order['phone'],order['name'])
         items=itertools.chain.from_iterable((item['name'],item['price']) for item in order['items'])
-        # print(list(items))
         for item in order["items"]:
             item_name = item["name"]
             items_info.setdefault(item_name, {"price": item["price"], "orders": 0})
             items_info[item_name]["orders"] += 1
     add_items(items_info)
-    #    items = order['items']
-    #     for item in items:
-    #         # print(item)    
-    #         itemname=item['name']
-    #         itemprice=item['price']
-    #         print(f"ItemName extracted from order: {itemname}")
-    #         print(f"ItemPrice extracted from order: {itemprice}")
-    #         add_items(itemname,itemprice)
 
-
-
+# Function to add customer information to a JSON file
 def add_customer(phone_number, customer_name):
     file_path = "customers.json"
 
@@ -44,10 +34,8 @@ def add_customer(phone_number, customer_name):
     customers_data[phone_number] = customer_name
     with open(file_path, 'w') as file:
         json.dump(customers_data, file, indent=4)
-
-
-    print(f"Customer '{customer_name}' with phone number '{phone_number}' added successfully.")
      
+# Function to add item information to a JSON file
 def add_items(itemdata):
     file_path = "items.json"
 
@@ -58,17 +46,14 @@ def add_items(itemdata):
         with open(file_path, 'w') as file:
             json.dump(itemdata, file, indent=4)
 
-
+# Main execution block
 if __name__ == "__main__":
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description='Provide the Json File name')
     parser.add_argument('filename', type=str, help='Provide the filename containing json objects')
-
+    # Read orders data from the provided file
     args = parser.parse_args()
     orders_data = read_json_from_file(args.filename)
-    items_info = {}
-
-
     if orders_data:
-        print("Orders read successfully:")
-        print(json.dumps(orders_data, indent=2))
+        # Extract and process data from orders
         extractingdata(orders_data)
